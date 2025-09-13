d8a6d8db4ed942d8b8154754ecdd714ab8f192e9
Modson Tande <131668620+Modsontech45@users.noreply.github.com>
2025-09-13 01:17:29 +0000

    hello

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:8080",
+  "capacitor://localhost",
   "http://127.0.0.1:5500",
   "http://localhost:5173",
   "https://besmart-delta.vercel.app",
